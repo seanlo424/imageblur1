8d8af6e819ed43d32035d5b7ba255ce440b19020
Sean O'Neil <sean.oneil424@gmail.comgit config --global user.name Sean>
2019-08-08 05:28:01 +0000

    image blur update

---
+++ imageblur.rb
@@ -0,0 +1,21 @@
+class Image
+	attr_accessor :row
+
+
+def initialize (row)
+	@row = row
+end
+
+def output_image
+	@row.each do |row|
+		puts row.join
+	end
+end
+
+image = Image.new([
+[0, 0, 0, 0],
+[0, 1, 0, 0],
+[0, 0, 0, 1],
+[0, 0, 0, 0]
+])
+image.output_image
