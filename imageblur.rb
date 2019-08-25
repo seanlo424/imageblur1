d87d1faae27cd0a30870853029f610c2910e4a8c
Sean O'Neil <sean.oneil424@gmail.comgit config --global user.name Sean>
2019-08-25 22:33:39 +0000

    imageblur2

--- imageblur.rb
+++ imageblur.rb
@@ -1,7 +1,6 @@
 class Image
 	attr_accessor :image
 
-
 	def initialize (image)
 		@image = image
 	end
@@ -9,8 +8,28 @@ class Image
 	def output_image
 		@image.each do |row|
 			puts row.join
-		end
-	end
+    end
+  end
+
+  def blur 
+  	bright_pixels = []
+  	@image.each_with_index do |row, row_index|
+  		row.each_with_index do |pixel, pixel_index| 
+  			if pixel == 1
+  				bright_pixels.push([row_index, pixel_index])
+				end
+			end
+  	end
+
+  	bright_pixels.each do |location|
+  		@image[location[0]][location[1] + 1] = 1 if location[1] + 1 <= @image[location[0]].length - 1
+  		@image[location[0]][location[1] - 1] = 1 if location[1 - 1 >= 0]
+  		@image[location[0] + 1][location[1]] = 1 if location[0] + 1 <= @image.length - 1
+  		@image[location[0] - 1][location[1]] = 1 if location[0] - 1 >= 0
+  	end
+
+
+  end
 end
 image = Image.new([
 [0, 0, 0, 0],
@@ -18,5 +37,6 @@ image = Image.new([
 [0, 0, 0, 1],
 [0, 0, 0, 0]
 ])
+image.bright_pixels!(2)
 image.output_image
 
